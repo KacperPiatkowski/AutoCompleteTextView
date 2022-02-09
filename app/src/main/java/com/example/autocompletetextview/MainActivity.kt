4c7bbdb9f0988c8d581c9c028191a377141ad3a6
KacperPiatkowski <k.piatkowski07@gmail.com>
2022-02-09 09:03:50 +0100

    nieskonczone

---
+++ app/src/main/java/com/example/autocompletetextview/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.autocompletetextview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        class MainActivity : AppCompatActivity() {    private
+        lateinit var binding: ActivityMainBinding    override
+            fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                binding =
+                    ActivityMainBinding.inflate(layoutInflater)        val
+                        view = binding.root        setContentView(view)    } }
+            }
+}
